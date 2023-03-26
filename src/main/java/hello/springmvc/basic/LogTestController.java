15c4f9213c569042d3e8a9976150418c63684aae
roberniro <denirokr22@gmail.com>
2023-03-26 19:13:47 +0900

    Spring Boot 로깅 기능 실습 @Slf4j
    <로그 장점>
    부가 정보, 출력 모양
    로그 레벨 설정
    콘솔 외 별도 위치(네트워크 전송, 파일)
    성능

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,14 +1,16 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController // 반환값이 String이어도 HTTP 메시지 바디에 바로 입력
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
@@ -16,6 +18,7 @@ public class LogTestController {
 
         System.out.println("name = " + name); // 로그 레벨 상관없이 무조건 출력, 로그로 부적절
 
+//        log.trace("trace my log" + name); // 미리 문자열을 더해 메모리 할당해 비효율적
         log.trace("trace log = {}", name);
         log.debug("debug log = {}", name);
         log.info("info log = {}", name);
