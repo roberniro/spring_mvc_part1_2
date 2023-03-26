5124515c1c4121a4faada5b405145635d7ea589e
roberniro <denirokr22@gmail.com>
2023-03-26 19:08:19 +0900

    Spring Boot 로깅 기능 실습 Logger

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 반환값이 String이어도 HTTP 메시지 바디에 바로 입력
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name); // 로그 레벨 상관없이 무조건 출력, 로그로 부적절
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+}
