b666baf5e366cb0c3bdc042cc6c0dee65426339d
roberniro <denirokr22@gmail.com>
2023-03-26 20:00:10 +0900

    Spring MVC 요청 매핑 API 실습

---
+++ src/main/java/hello/springmvc/basic/requesmapping/MappingClassController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.requesmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class MappingClassController {
+
+    @GetMapping("/users")
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping("/users")
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/users/{userid}")
+    public String findUser(@PathVariable String userid) {
+        return "get userid = " + userid;
+    }
+
+    @PatchMapping("/users/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userid = " + userId;
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userid = " + userId;
+    }
+}
